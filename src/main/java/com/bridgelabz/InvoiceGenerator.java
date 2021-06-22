cbc9892a01240438cdc0984377626b20e262899b
shwetap29 <shwetaprasad48@gmail.com>
2021-06-22 09:52:55 +0530

    ADDED:Test cases to check enhancedInvoice

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -6,13 +6,15 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME =1 ;
     private static final double MINIMUM_FARE = 5;
 
+
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
-    public double calculateFare(Ride[] rides){
+
+    public double calculateFare(Ride[] rides) {
         double totalFare = 0;
-        for (Ride ride : rides){
+        for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return totalFare;
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -5,10 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+
     InvoiceGenerator invoiceGenerator = null;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         invoiceGenerator = new InvoiceGenerator();
     }
 
@@ -30,11 +31,25 @@ public class InvoiceServiceTest {
     }
 
     @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1) , new Ride (5.0, 4)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(84, fare, 0.0);
+    }
+
+    @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        Ride[] rides = { new Ride(25, 30), new Ride(12, 20) };
+        Ride[] rides = { new Ride(25, 30),
+                new Ride(12, 20)
+        };
         InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
         InvoiceSummary summary = new InvoiceSummary(2, 420);
         Assert.assertEquals(summary, invoiceSummary);
     }
+
     }
+
 
